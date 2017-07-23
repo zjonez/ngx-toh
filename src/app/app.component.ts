8a270a58412ab4d50445498ca19beb8d0cf9c067
Jonas Zapanta <jonas.zapanta@gmail.com>
2017-07-23 14:53:46 +0800

    lesson 2: hero editor complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,23 +3,20 @@ import { Hero } from '../hero/hero';
 
 @Component({
   selector: 'app-root',
-  // templateUrl: './app.component.html',
-  template: `
-    <h1>{{title}}</h1>
-    <h2>
-      <b>id: </b> {{hero.id}} <br>
-      <b>name: </b> {{hero.name}} <br>
-      {{hero | json}}
-    </h2>
-  `,
+  templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 
 export class AppComponent {
   title = 'tour of shit';
-  
+
   hero: Hero = {
     id: 1,
-    name: 'juancho'
+    name: 'JM TANGAs'
   }
+  // hero: Hero = {
+  //   id: null,
+  //   // name: 'juancho'
+  //   name: ''
+  // }
 }
