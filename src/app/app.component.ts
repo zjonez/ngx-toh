fcd7510ad46a58e8b268e56d6e486629d3b2f90e
Jesse <jesse.dino.z3@gmail.com>
2017-07-26 20:02:50 +0800

    lesson2: hero editor:

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,31 @@
 import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
-})
-export class AppComponent {
-  title = 'shit of tour';
-  hero: Hero = {
-    id: 1,
-    name: `tae`
-  }
-}
-
+     
 export class Hero {
   id: number;
   name: string;
 }
+     
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html'
+})
+export class AppComponent {
+  title = 'HoN';
+  hero: Hero = {
+  id: 1,  
+  name: 'Valk'
+  };
+}
+
+const HEROES: Hero[] = [
+  { id: 11, name: 'Empath' }, 
+  { id: 12, name: 'Pebbles' },
+  { id: 13, name: 'Predator' },
+  { id: 14, name: 'Chronos' },
+  { id: 15, name: 'Slither' },
+  { id: 16, name: 'Chipper' },
+  { id: 17, name: 'Pyromancer' },
+  { id: 18, name: 'Witch Hunter' },
+  { id: 19, name: 'Magnus' },
+  { id: 20, name: 'Zephyr' }
+];
