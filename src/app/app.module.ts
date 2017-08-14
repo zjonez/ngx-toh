332063f0f70e0b531f55c879365cda7ac1f32ff7
Juancho Tupas <tupasjuancho@gmail.com>
2017-08-14 22:45:27 +0800

    Routing Lesson Partial Code Update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+
 import { AppComponent } from './app.component';
 import { HeroDetailComponent } from './hero-detail.component';
 import { HeroesComponent } from './heroes.component';
 import {HeroService} from './hero.service'
-import { RouterModule } from '@angular/router';
 import { DashboardComponent } from './dashboard.component'
+
+import { AppRoutingModule } from './app-routing.module';
 
 
 @NgModule({
@@ -19,29 +21,11 @@ import { DashboardComponent } from './dashboard.component'
   imports: [
     BrowserModule,
     FormsModule,
-    RouterModule.forRoot([
-      {
-        path: 'heroes',
-        component: HeroesComponent
-      },
-      {
-        path: 'dashboard',
-        component: DashboardComponent
-      },
-      {
-        path: '',
-        redirectTo: '/dashboard',
-        pathMatch: 'full'
-      },
-    ])
+    AppRoutingModule
   ],
-  providers: [
-    HeroService
-  ],
-  bootstrap: [AppComponent]
+  providers: [HeroService],
+  bootstrap: [ AppComponent ]
 })
-export class AppModule { 
-
-}
+export class AppModule {}
 
 
